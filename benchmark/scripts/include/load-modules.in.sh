34d287ce02f5d315c4c23479dcd966a57f8ecf3f
Serdar Bulut <sbulut@gmail.com>
2024-04-06 16:26:04 -0700

    Merge changes to get LSMIO to be built on ISAMBARD.

--- benchmark/scripts/include/load-modules.in.sh
+++ benchmark/scripts/include/load-modules.in.sh
@@ -28,11 +28,36 @@ load_modules_viking2() {
 }
 
 load_modules_isambard() {
-  module load hdf5/1.10.1
-  module load gcc/9.2.0
-  module load openmpi/gcc/64/1.10.7
-  module load boost/1.71.0
-  module load fftw3/openmpi/gcc/64/3.3.8
+  module load modules/3.2.11.4
+  module load system-config/3.6.3070-7.0.2.1_7.3__g40f385a9.ari
+  module load craype-network-aries
+  module load Base-opts/2.4.142-7.0.2.1_2.69__g8f27585.ari
+  module load alps/6.6.59-7.0.2.1_3.62__g872a8d62.ari
+  # module load cce/11.0.4
+  module load nodestat/2.3.89-7.0.2.1_2.53__g8645157.ari
+  module load craype/2.6.2
+  module load sdb/3.3.812-7.0.2.1_2.74__gd6c4e58.ari
+  module load cray-libsci/20.09.1
+  module load udreg/2.3.2-7.0.2.1_2.24__g8175d3d.ari
+  module load pmi/5.0.17
+  module load ugni/6.0.14.0-7.0.2.1_3.24__ge78e5b0.ari
+  module load atp/3.11.7
+  module load gni-headers/5.0.12.0-7.0.2.1_2.27__g3b1768f.ari
+  module load rca/2.2.20-7.0.2.1_2.76__g8e3fb5b.ari
+  module load dmapp/7.1.1-7.0.2.1_2.75__g38cf134.ari
+  module load perftools-base/21.05.0
+  module load xpmem/2.2.20-7.0.2.1_2.61__g87eb960.ari
+  #module load PrgEnv-cray/6.0.9
+  module load llm/21.4.629-7.0.2.1_2.53__g8cae6ef.ari
+  module load cray-mpich/7.7.17
+  #  Additions
+  module load gcc/10.3.0
+  module load tools/cmake/3.24.2
+  module load PrgEnv-gnu/6.0.9
+  #module load cray-mpich/7.7.17
+  module load cray-mpich-abi/7.7.17
+  module load cray-hdf5-parallel/1.12.0.4
+  module load cray-fftw/3.3.8.10
 }
 
 
