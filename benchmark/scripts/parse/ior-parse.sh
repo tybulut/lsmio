e379f7e44b65507f386db4a43fe7cdcb0e282074
Serdar Bulut <sbulut@gmail.com>
2024-04-28 10:11:18 -0700

    fix IOR parser issue with the new output file name due to new HPC support (#24)

--- benchmark/scripts/parse/ior-parse.sh
+++ benchmark/scripts/parse/ior-parse.sh
@@ -17,7 +17,9 @@ do
     | sed "s|$IOR_DIR_OBASE/||" \
     | sed "s|/20..-..-..||" \
     | sed "s|-20..-..-..-node...-0.txt||" \
-    | perl -e 's/^(.|..)\/out-[a-z]+-(\d+)-(\d+[MK])/$1  $2  $3/' -p \
+    | sed "s|-20..-..-..-nid.....-0.txt||" \
+    | perl -e 's/^(.|..)\/out-[a-z0-9]+-(\d+)-(\d+[MK])/$1  $2  $3/' -p \
+    | perl -e 's/^(.|..)\/out-[a-z0-9]+-c-(\d+)-(\d+[MK])/$1  $2  $3/' -p \
     | perl -e 's/ +/,/g' -p \
     >> "$REPORT_FILE"
 done
