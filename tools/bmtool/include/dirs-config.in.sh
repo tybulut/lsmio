c3aa0f1b47f0a2897558c3e0e7565a54b19cfb0e
Serdar Bulut <sbulut@gmail.com>
2025-10-13 06:11:36 +0100

    cleanup

--- tools/bmtool/include/dirs-config.in.sh
+++ tools/bmtool/include/dirs-config.in.sh
@@ -1,24 +1,24 @@
 #
-if [ "$HPC_ENV" = "viking" ]; then
-  lfs setstripe -S 64K -c 4 $DIRS_BM_C4_B64
-  lfs setstripe -S 64K -c 16 $DIRS_BM_c16_B64
-  lfs setstripe -S 1M -c 4 $DIRS_BM_C4_B1M
-  lfs setstripe -S 1M -c 16 $DIRS_BM_c16_B1M
-  lfs setstripe -S 8M -c 4 $DIRS_BM_C4_B8M
-  lfs setstripe -S 8M -c 16 $DIRS_BM_c16_B8M
-elif [ "$BM_SSD" = "on" ]; then
+if [ "$HPC_ENV" = "viking2" && "$BM_SSD" = "on" ]; then
   lfs setstripe -S 64K -c 4 $DIRS_BM_C4_B64 -p scratch.flash
   lfs setstripe -S 64K -c 16 $DIRS_BM_c16_B64 -p scratch.flash
   lfs setstripe -S 1M -c 4 $DIRS_BM_C4_B1M -p scratch.flash
   lfs setstripe -S 1M -c 16 $DIRS_BM_c16_B1M -p scratch.flash
   lfs setstripe -S 8M -c 4 $DIRS_BM_C4_B8M -p scratch.flash
   lfs setstripe -S 8M -c 16 $DIRS_BM_c16_B8M -p scratch.flash
-else
+elif [ "$HPC_ENV" = "viking2" ]; then
   lfs setstripe -S 64K -c 4 $DIRS_BM_C4_B64 -p scratch.disk
   lfs setstripe -S 64K -c 16 $DIRS_BM_c16_B64 -p scratch.disk
   lfs setstripe -S 1M -c 4 $DIRS_BM_C4_B1M -p scratch.disk
   lfs setstripe -S 1M -c 16 $DIRS_BM_c16_B1M -p scratch.disk
   lfs setstripe -S 8M -c 4 $DIRS_BM_C4_B8M -p scratch.disk
   lfs setstripe -S 8M -c 16 $DIRS_BM_c16_B8M -p scratch.disk
+else
+  lfs setstripe -S 64K -c 4 $DIRS_BM_C4_B64
+  lfs setstripe -S 64K -c 16 $DIRS_BM_c16_B64
+  lfs setstripe -S 1M -c 4 $DIRS_BM_C4_B1M
+  lfs setstripe -S 1M -c 16 $DIRS_BM_c16_B1M
+  lfs setstripe -S 8M -c 4 $DIRS_BM_C4_B8M
+  lfs setstripe -S 8M -c 16 $DIRS_BM_c16_B8M
 fi
 #
