1fe5abdf08335627412b494c43325eea238e8544
Serdar Bulut <sbulut@gmail.com>
2026-09-24 11:03:24 -0700

    build-tlx

---
+++ doc/dependencies/06-build-tlx.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -x
+
+PREFIX="${PREFIX:-${PROJECT_DIR:-$HOME/src/usr}}"
+
+mkdir -p $HOME/src/packages/06-tlx
+cd $HOME/src/packages/06-tlx
+
+git clone https://github.com/tlx/tlx.git
+
+cd tlx
+git checkout v0.6.1
+cd ..
+
+rm -rf tlx-build
+mkdir tlx-build
+
+cd tlx-build
+cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_SHARED_LIBS=On \
+  -DTLX_BUILD_TESTS=OFF \
+  -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+  ../tlx
+
+make -j16 \
+&& make all install
